d077e1ca9e51940bc636b56ee825a9c390dd8093
James <james@ancestorcloud.com>
2016-02-12 13:48:47 -0700

    Add suffix to degree

--- src/App/views/Result/index.js
+++ src/App/views/Result/index.js
@@ -6,10 +6,26 @@ import TrumpConnection from './components/TrumpConnection'
 import Footer from 'org.Footer'
 import Btn from 'atm.Btn'
 
+/**
+ * 1. round down to nearest ten
+ * 2. get number in unit column
+ */
+const getSuffix = (number) => {
+  const unit = number - (Math.round(number / 10) * 10 /* 1 */) /* 2 */
+  return unit === 1
+  ? 'st'
+  : unit === 2
+  ? 'nd'
+  : unit === 3
+  ? 'rd'
+  : 'th'
+}
+
 const Result = ({
   session: {
     user
-  }
+  },
+  degrees
 }) => (
   <div className={style.wrapper}>
     <div className={style.main}>
@@ -17,11 +33,11 @@ const Result = ({
       <h2>Well would you look at that?</h2>
       <TrumpConnection
         avatarSrc={user && user.picture && user.picture.data && user.picture.data.url}
-        degrees={27}
+        degrees={degrees}
       />
     </div>
     <div className={style.explainer}>
-      <h2>You are 27th cousins with Donald</h2>
+      <h2>You are {degrees}{getSuffix(degrees)} cousins with Donald</h2>
       <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur. Donec ut libero sed arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut gravida lorem. Ut turpis felis, pulvinar a semper sed, adipiscing id dolor. Pellentesque auctor nisi id magna consequat sagittis. Curabitur dapibus enim sit amet elit pharetra tincidunt feugiat nisl imperdiet. Ut convallis libero in urna ultrices accumsan. Donec sed odio eros. Donec viverra mi quis quam pulvinar at malesuada arcu rhoncus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In rutrum accumsan ultricies. Mauris vitae nisi at sem facilisis semper ac in est.</div>
       <div className={style.buttonsWrapper}>
         <div>
@@ -45,13 +61,15 @@ Result.propTypes = {
 }
 
 export default connect(({session}) => ({
+  // sample data
   session: {
-    user: { // sample data
+    user: {
       picture: {
         data: {
           url: 'https:\/\/scontent.xx.fbcdn.net\/hprofile-xfa1\/v\/t1.0-1\/s200x200\/10354686_10150004552801856_220367501106153455_n.jpg?oh=84e7999c372de15d97a6043b6c81f35c&oe=57378050'
         }
       }
     }
-  }
+  },
+  degrees: 27
 }))(Result)
