487b5d989a1f98711ce7933660c74b1345e0edbb
James <james@ancestorcloud.com>
2016-02-13 12:25:33 -0700

    Loading indicator on Result page

--- src/App/views/Result/index.js
+++ src/App/views/Result/index.js
@@ -20,45 +20,65 @@ const getSuffix = (number) => {
   return suffixMap[unit] || 'th'
 }
 
-const Result = ({
-  session: {
-    user
+const Result = React.createClass({
+  getInitialState () {
+    return {
+      degreesPlaceholder: false
+    }
   },
-  degrees
-}) => (
-  <div className={style.wrapper}>
-    <div className={style.main}>
-      <img src='/images/stars.svg' />
-      <h2>Well would you look at that?</h2>
-      <TrumpConnection
-        avatarSrc={user && user.picture && user.picture.data && user.picture.data.url}
-        degrees={degrees}
-        size='big'
-      />
-    </div>
-    <div className={style.explainer}>
-      <h2>You are {degrees}{getSuffix(degrees)} cousins with Donald</h2>
-      <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur. Donec ut libero sed arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut gravida lorem. Ut turpis felis, pulvinar a semper sed, adipiscing id dolor. Pellentesque auctor nisi id magna consequat sagittis. Curabitur dapibus enim sit amet elit pharetra tincidunt feugiat nisl imperdiet. Ut convallis libero in urna ultrices accumsan. Donec sed odio eros. Donec viverra mi quis quam pulvinar at malesuada arcu rhoncus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In rutrum accumsan ultricies. Mauris vitae nisi at sem facilisis semper ac in est.</div>
-      <div className={style.buttonsWrapper}>
-        <div>
-          <Btn
-            copy='Share your cousin'
-          />
-        </div>
-        <div>
-          <Btn
-            copy='Share your cousin'
-          />
-        </div>
-      </div>
-    </div>
-    <Footer />
-  </div>
-)
 
-Result.propTypes = {
-  specs: PropTypes.array
-}
+  render () {
+    const {session: {user}, degrees} = this.props
+    const { degreesPlaceholder } = this.state
+    if (!degreesPlaceholder) {
+      console.log('starting timer')
+      setTimeout(() => {
+        console.log('setting degrees')
+        this.setState({degreesPlaceholder: 27})
+      }, 8000)
+    }
+
+    return (
+      <div className={style.wrapper}>
+        <div className={style.main}>
+          <img src='/images/stars.svg' />
+          <h2>{degreesPlaceholder ? 'Well would you look at that?' : 'Calculating...'}</h2>
+          <TrumpConnection
+            avatarSrc={user && user.picture && user.picture.data && user.picture.data.url}
+            degrees={degreesPlaceholder}
+            size='big'
+            loading={!degreesPlaceholder}
+          />
+        </div>
+        {
+          degreesPlaceholder
+          ? (
+            <div className={style.explainerWrapper}>
+              <div className={style.explainer}>
+                <h2>You are {degreesPlaceholder}{getSuffix(degreesPlaceholder)} cousins with Donald</h2>
+                <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur. Donec ut libero sed arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut gravida lorem. Ut turpis felis, pulvinar a semper sed, adipiscing id dolor. Pellentesque auctor nisi id magna consequat sagittis. Curabitur dapibus enim sit amet elit pharetra tincidunt feugiat nisl imperdiet. Ut convallis libero in urna ultrices accumsan. Donec sed odio eros. Donec viverra mi quis quam pulvinar at malesuada arcu rhoncus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In rutrum accumsan ultricies. Mauris vitae nisi at sem facilisis semper ac in est.</div>
+                <div className={style.buttonsWrapper}>
+                  <div>
+                    <Btn
+                      copy='Share your cousin'
+                    />
+                  </div>
+                  <div>
+                    <Btn
+                      copy='Share your cousin'
+                    />
+                  </div>
+                </div>
+                <Footer />
+              </div>
+            </div>
+          )
+          : undefined
+        }
+      </div>
+    )
+  }
+})
 
 export default connect(({session}) => ({
   // sample data
@@ -66,10 +86,10 @@ export default connect(({session}) => ({
     user: {
       picture: {
         data: {
-          url: 'https:\/\/scontent.xx.fbcdn.net\/hprofile-xfa1\/v\/t1.0-1\/s200x200\/10354686_10150004552801856_220367501106153455_n.jpg?oh=84e7999c372de15d97a6043b6c81f35c&oe=57378050'
+          url: 'http://i.imgur.com/lEzM7g8.jpg'
         }
       }
     }
   },
-  degrees: 27
+  degrees: false
 }))(Result)
