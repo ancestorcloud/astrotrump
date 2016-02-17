606fe33e8fad2a9792a4131e1b675c76ffb27c0d
James <james@ancestorcloud.com>
2016-02-16 18:17:10 -0700

    Analytics

--- html.js
+++ html.js
@@ -21,7 +21,7 @@ module.exports = {
     var config = {
       title: 'AncestorCloud',
       favicon: '/assets/icons/favicon.ico',
-      gaId: ''
+      gaId: 'UA-47141819-6'
     }
 
     return {
@@ -41,13 +41,13 @@ module.exports = {
             '<link rel="icon" href="' + config.favicon + '" type="image/x-icon" />',
             '<link href="/' + data.css + '" rel="stylesheet" type="text/css" />',
             '<link href="http://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet" type="text/css">',
-            '<script>' +
-              '(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){' +
-              '(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),' +
-              'm=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)' +
-              '})(window,document,"script","//www.google-analytics.com/analytics.js","ga");  ga("create", "UA-47141819-6", "auto");' +
-              'ga("send", "pageview");' +
-            '</script>',
+
+            '<script>!function(m,i,k,e,y){m.GoogleAnalyticsObject=k;m[k]||(m[k]=function(){' +
+            '(m[k].q=m[k].q||[]).push(arguments)});m[k].l=+new Date;e=i.createElement("script");' +
+            'y=i.scripts[0];e.src="//www.google-analytics.com/analytics.js";' +
+            'y.parentNode.insertBefore(e,y)}(window,document,"ga");' +
+
+            'ga("create", "' + config.gaId + '", "auto");</script>',
           '</head>',
           '<body>',
             '<div id="app"></div>',
