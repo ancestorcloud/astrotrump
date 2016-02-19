7e0faec69f1d8f75293d86013e8b4e3534c9fbb3
Mikey Murphy <mikeyamadeo@gmail.com>
2016-02-19 10:26:26 -0700

    fonts

--- html.js
+++ html.js
@@ -41,7 +41,7 @@ module.exports = {
             '<link rel="icon" href="' + config.favicon + '" type="image/x-icon" />',
             '<link rel="image_src" href="/images/are_you_related_trump.jpg" / >',
             '<link href="/' + data.css + '" rel="stylesheet" type="text/css" />',
-            '<link href="http://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet" type="text/css">',
+            '<link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet" type="text/css">',
             '<script>(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){' +
             '(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),' +
             'm=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)' +
